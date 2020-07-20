707b954a905757eaa6c82750082f2fcac0bcad2c
Lukas Berbuer <lukas.berbuer@gmail.com>
2020-07-20 16:16:52 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"] + EXTRAS_REQUIRE["tools"]
 
 setup(
     name="daqmon",
-    version="0.1.0",
+    version="0.2.0",
     description="DaqMon (Data Acquisition and Monitoring) interface for Vallen Systeme dashboards",
     author="Lukas Berbuer (Vallen Systeme GmbH)",
     author_email="software@vallen.de",
